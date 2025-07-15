57dfac9029a381a2c2a8ec4b87f8a0fbfe37f440
Rina Bard <katrinbardal@gmail.com>
2025-07-15 14:32:19 +0300

    feat: Added icons consts

---
+++ lib/src/common/consts/icons_consts.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class IconsConsts {
+  static const IconData home = CupertinoIcons.home;
+  static const IconData search = CupertinoIcons.search;
+  static const IconData favorite = CupertinoIcons.heart;
+  static const IconData profile = CupertinoIcons.person;
+  static const IconData language = CupertinoIcons.globe;
+  static const IconData filter = CupertinoIcons.slider_horizontal_3;
+  static const IconData close = CupertinoIcons.clear;
+  static const IconData edit = CupertinoIcons.pencil;
+  static const IconData trash = CupertinoIcons.trash;
+  static const IconData done = CupertinoIcons.check_mark;
+}
